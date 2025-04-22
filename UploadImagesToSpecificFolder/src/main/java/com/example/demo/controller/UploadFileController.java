9a8b9480826800123865cbd0b47bd072cf808740
shekharpokharkar <132981368+shekharpokharkar@users.noreply.github.com>
2025-04-22 09:41:45 +0530

    Update UploadFileController.java

--- UploadImagesToSpecificFolder/src/main/java/com/example/demo/controller/UploadFileController.java
+++ UploadImagesToSpecificFolder/src/main/java/com/example/demo/controller/UploadFileController.java
@@ -18,8 +18,8 @@ public class UploadFileController {
 
 	@PostMapping("/upload")
 	public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
-		System.out.println("********++++++++++++++++++*********");
-		System.out.println("hello Good Morning");
+		System.out.println("****>>>>>>>>>>>>>>>>>>>>>>>>>****");
+		System.out.println("=============================================");
 		String uploadFile = service.uploadFile(file);
 		return new ResponseEntity<>(uploadFile, HttpStatus.CREATED);
 	}
